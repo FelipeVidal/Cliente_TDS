eac4fab706ec94eab44fa01cf7c16d077a1a2fc6
FELIPE <felipevidalsan@gmail.com>
2020-10-06 18:25:48 -0500

    arquitecturaCliente

---
+++ src/servicios/servicioServidorTcpSecuencial.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servicios;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+/**
+ *
+ * @author Felipe
+ */
+public class servicioServidorTcpSecuencial {
+  public void conectServer(String ipServer, int PuertoServer){
+              //Host del servidor
+        final String dirIpServidor = "127.0.0.1";
+        //Puerto del servidor
+        final int PUERTO_SERVIDOR = 5000;
+        DataInputStream objFlujoDeEntrada;
+        DataOutputStream objFlujoDeSalida;
+ 
+        try {
+            //Creo el socket para conectarme con el servidor
+            Socket objSocket = new Socket(dirIpServidor, PUERTO_SERVIDOR);
+ 
+            objFlujoDeEntrada = new DataInputStream(objSocket.getInputStream());
+            objFlujoDeSalida = new DataOutputStream(objSocket.getOutputStream());
+ 
+            //El Cliente envia un mensaje al servidor
+            objFlujoDeSalida.writeUTF("¡Hola mundo desde el cliente!");
+ 
+            //El Cliente recibe un mensaje del servidor
+            String mensaje = objFlujoDeEntrada.readUTF();
+ 
+            System.out.println(mensaje);
+            
+            //Cierro el socket
+            objSocket.close();
+ 
+        } catch (IOException ex) {
+            System.out.println("Error al realizar la conexión");
+        }
+  }  
+  
+}
